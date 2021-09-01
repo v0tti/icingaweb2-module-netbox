63a9b7205dd89c213d176d9279a51016568c742d
Alexander Votteler <avotteler@relaix.net>
2021-09-01 10:58:00 +0200

    Rearrange static parameters in function call

--- library/Netbox/ProvidedHook/Director/ImportSource.php
+++ library/Netbox/ProvidedHook/Director/ImportSource.php
@@ -217,7 +217,7 @@ class ImportSource extends ImportSourceHook
 		switch ($mode) {
 			// VM's
 			case self::VMMode:
-				$services = $netbox->allservices(0, "");
+				$services = $netbox->allservices("", 0);
 				$devices = $netbox->virtualMachines($filter, $limit);
 				return $this->devices_with_services($services, $devices);
 			case self::ClusterMode:
@@ -229,7 +229,7 @@ class ImportSource extends ImportSourceHook
 							
 			// Device
 			case self::DeviceMode:
-				$services = $netbox->allservices(0, "");
+				$services = $netbox->allservices("", 0);
 				$devices = $netbox->devices($filter, $limit);
 				return $this->devices_with_services($services, $devices, $filter);
 			case self::DeviceRoleMode:
